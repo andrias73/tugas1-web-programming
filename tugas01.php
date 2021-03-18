f9c94a4448cb25bc16e0e1a6a703fe0d371e663e
andrias ari ardiyanto <62227514+andrias73@users.noreply.github.com>
2021-03-18 13:53:37 +0700

    Add files via upload

---
+++ tugas01.php
@@ -0,0 +1,14 @@
+<?php
+ //12191662_ Andrias
+ 
+$ikanku = array("Teri","Lele","Bawal","Tuna");
+	echo "ikan $ikanku[0] sering dijumpai di nasi kucing angkringan dan saya konsumsi pas tanggal tua.
+	Kalo tanggal muda sering kuliner $ikanku[1],$ikanku[2], dan $ikanku[3]";
+	echo "<br>"
+	echo "<br>"	
+?>
+
+<?php
+$date = "2017-03-14";
+echo date ("d-m-y", strtotime ($date));
+?>
